c129390cf062492b7476a46cdb55068ebcd4e03a
JB Tellez <jb.tellez@gmail.com>
2023-04-11 07:20:00 -0700

    updates class 2 demo

--- class-02/demo/beast-judge/src/App.js
+++ class-02/demo/beast-judge/src/App.js
@@ -1,9 +1,9 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container'
-import Header from './components/header'
-import Footer from './components/footer'
-import Main from './components/main'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
 import imageUrls from './data.json'
 
 function App() {
